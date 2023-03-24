5b05920fbb1e9ef3ab64577700339c72d69d72af
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-03-24 12:41:19 +0000

    create event info table wit backend point

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ server.get("/gift", getGiftsFromDBHandler);
 ///////////////delete from DataBase gift////////////
 server.delete("/gift/:id", deleteFromDbHandler);
 
+
+server.get('/events',getEvent)
+server.post('/events',addEvent)
+server.put('/events/:id',updateEvent)
+server.delete('/events/:id',deleteEvent)
+
 server.use(errorHandler); // under all routs
 ///////////////////////////////////////////////////////// Music constructor /////////////////////////////////////////////////////////////////////
 function Music(track_name, track_url, aritst_name) {
@@ -236,6 +242,69 @@ function giftsAPIHandler(req, res) {
         errorHandler(err, req, res);
       });
   }
+
+// //////////////////////////////////////////////////  event information part /////////////////////////////////////
+
+function addEvent(req,res){
+  const data=req.body ;
+  console.log(data)
+  const sql=`INSERT INTO eventinfo (event, location,date,description) VALUES ($1,$2,$3,$4,$5) RETURNING *;`
+  const values = [data.event, data.location, data.date ,data.description,data.user_email];
+  client.query(sql,values)
+  .then((data) => {
+      res.send("your data was added !");
+  })
+      .catch(error => {
+          // console.log(error);
+          errorHandler(error, req, res);
+      });
+
+}
+
+function updateEvent(req,res){
+
+  const event_id=req.params.id ;
+  const data=req.body ;
+  
+ 
+  const sql=`UPDATE eventinfo SET event= $1 , location= $2 , date=$3 , description= $4 ,user_email= $5 WHERE id=${event_id} ;`;
+  const values = [data.event, data.location, data.date ,data.description,data.user_email];
+    
+
+  client.query(sql,values)
+      .then((data)=>{
+           res.status(200).send(data.rows);
+      })
+      .catch((err)=>{
+          console.log(err)
+       })
+
+}
+
+function deleteEvent(req,res){
+  const id=req.params.id ;
+  const sql = `DELETE FROM eventinfo WHERE id=${id}`;
+  client.query(sql)
+  .then((data)=>{
+      res.status(204).json({});
+  })
+  .catch((err)=>{
+      errorHandler(err,req,res);
+  })
+}
+
+function getEvent(req,res){
+  const sql = `SELECT * FROM eventinfo`;
+  client.query(sql)
+  .then((data)=>{
+      res.send(data.rows);
+  })
+  .catch((err)=>{
+      errorHandler(err,req,res);
+  })
+
+}
+
 /////////////////////////////////////////////////////////////// error Handler /////////////////////////////////////////////////////////////////
 function errorHandler(error, req, res) {
     const err = {
